351b4aae06faf501ae8ff780ab46d3b9c6b0ffd2
Omid Tavakoli <omidtavakoli.info@gmail.com>
2019-10-25 13:24:08 +0330

    resolve url

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hugo-coder # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
@@ -22,4 +22,4 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master./de
+git push origin master
